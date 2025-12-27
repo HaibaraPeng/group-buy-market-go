e72f926b72a47eadc2be248685fc150b5ceeaac9
Roc <514178739@qq.com>
2025-12-27 14:34:52 +0800

    refactor(common): 重命名Model2交易规则工厂类

--- internal/common/design/link/model2/model2_trade_rule_factory.go
+++ internal/common/design/link/model2/model2_trade_rule_factory.go
@@ -1,6 +1,6 @@
 package model2
 
-// DynamicContext 对应Java版本中的Rule02TradeRuleFactory.DynamicContext类
+// DynamicContext 对应Java版本中的Model2TradeRuleFactory.DynamicContext类
 type DynamicContext struct {
 	Age string
 }
@@ -15,16 +15,16 @@ func (dc *DynamicContext) SetAge(age string) {
 	dc.Age = age
 }
 
-// Rule02TradeRuleFactory 是对应Java版本Rule02TradeRuleFactory类的Go实现
-type Rule02TradeRuleFactory struct{}
+// Model2TradeRuleFactory 是对应Java版本Model2TradeRuleFactory类的Go实现
+type Model2TradeRuleFactory struct{}
 
 // NewRule02TradeRuleFactory 创建一个新的工厂实例
-func NewRule02TradeRuleFactory() *Rule02TradeRuleFactory {
-	return &Rule02TradeRuleFactory{}
+func NewRule02TradeRuleFactory() *Model2TradeRuleFactory {
+	return &Model2TradeRuleFactory{}
 }
 
 // Demo01 创建第一个责任链实例，包含RuleLogic201和RuleLogic202
-func (rf *Rule02TradeRuleFactory) Demo01() *BusinessLinkedList[string, *DynamicContext, *XxxResponse] {
+func (rf *Model2TradeRuleFactory) Demo01() *BusinessLinkedList[string, *DynamicContext, *XxxResponse] {
 	ruleLogic201 := NewRuleLogic201()
 	ruleLogic202 := NewRuleLogic202()
 
@@ -37,7 +37,7 @@ func (rf *Rule02TradeRuleFactory) Demo01() *BusinessLinkedList[string, *DynamicC
 }
 
 // Demo02 创建第二个责任链实例，只包含RuleLogic202
-func (rf *Rule02TradeRuleFactory) Demo02() *BusinessLinkedList[string, *DynamicContext, *XxxResponse] {
+func (rf *Model2TradeRuleFactory) Demo02() *BusinessLinkedList[string, *DynamicContext, *XxxResponse] {
 	ruleLogic202 := NewRuleLogic202()
 
 	linkArmory := NewLinkArmory("demo02",
