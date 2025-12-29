680d3ff2e26b2cbcc6a106446d4ed7caa9ed8afb
Roc <514178739@qq.com>
2025-12-29 22:49:30 +0800

    fix(trade): 修复用户参与次数限制过滤器逻辑

--- internal/domain/trade/biz/filter/user_take_limit_rule_filter.go
+++ internal/domain/trade/biz/filter/user_take_limit_rule_filter.go
@@ -29,30 +29,29 @@ func NewUserTakeLimitRuleFilter(logger log.Logger, tradeRepository *repository.T
 
 // Next 实现责任链处理器接口
 func (f *UserTakeLimitRuleFilter) Next(ctx context.Context, command *model.TradeRuleCommandEntity, dynamicContext *DynamicContext) (*model.TradeRuleFilterBackEntity, error) {
-	return f.filter(ctx, command, dynamicContext)
+	return nil, nil
 }
 
 // Apply 实现责任链处理器接口
 func (f *UserTakeLimitRuleFilter) Apply(ctx context.Context, command *model.TradeRuleCommandEntity, dynamicContext *DynamicContext) (*model.TradeRuleFilterBackEntity, error) {
-	return f.filter(ctx, command, dynamicContext)
-}
+	f.log.Infow("交易规则过滤-用户参与次数校验", "userId", command.UserId, "activityId", command.ActivityId)
 
-// filter 是实际的过滤逻辑
-func (f *UserTakeLimitRuleFilter) filter(ctx context.Context, command *model.TradeRuleCommandEntity, dynamicContext *DynamicContext) (*model.TradeRuleFilterBackEntity, error) {
-	// 查询用户参与活动的订单量
-	userTakeOrderCount, err := f.tradeRepository.QueryOrderCountByActivityId(ctx, command.ActivityId, command.UserId)
+	// 从动态上下文获取活动信息
+	groupBuyActivity := dynamicContext.GroupBuyActivity
+
+	// 查询用户在一个拼团活动上参与的次数
+	count, err := f.tradeRepository.QueryOrderCountByActivityId(ctx, command.ActivityId, command.UserId)
 	if err != nil {
 		return nil, err
 	}
 
-	// 检查活动参与限制
-	if dynamicContext.GroupBuyActivity != nil && userTakeOrderCount >= dynamicContext.GroupBuyActivity.TakeLimitCount {
-		// 用户参与次数达到限制，抛出异常
+	if groupBuyActivity != nil && groupBuyActivity.TakeLimitCount != 0 && count >= groupBuyActivity.TakeLimitCount {
+		f.log.Infow("用户参与次数校验，已达可参与上限", "activityId", command.ActivityId)
 		return nil, exception.NewAppException(consts.E0103)
 	}
 
 	// 用户未达到参与限制，继续执行下个过滤器
 	return &model.TradeRuleFilterBackEntity{
-		UserTakeOrderCount: userTakeOrderCount,
+		UserTakeOrderCount: count,
 	}, nil
 }
