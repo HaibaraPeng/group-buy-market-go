35064162264638d4fa8a992c668256defbaf5525
Roc <514178739@qq.com>
2025-12-30 22:47:27 +0800

    feat(trade): 添加拼团交易结算服务

--- internal/domain/trade/biz/settlement/trade_settlement_order.go
+++ internal/domain/trade/biz/settlement/trade_settlement_order.go
@@ -1,1 +1,65 @@
-package lock
+package settlement
+
+import (
+	"context"
+	"log"
+
+	"group-buy-market-go/internal/domain/trade/model"
+	"group-buy-market-go/internal/infrastructure/adapter/repository"
+)
+
+// TradeSettlementOrderService 拼团交易结算服务
+type TradeSettlementOrderService struct {
+	repository *repository.TradeRepository
+}
+
+// NewTradeSettlementOrderService 创建拼团交易结算服务实例
+func NewTradeSettlementOrderService(repository *repository.TradeRepository) *TradeSettlementOrderService {
+	return &TradeSettlementOrderService{
+		repository: repository,
+	}
+}
+
+// SettlementMarketPayOrder 拼团交易结算
+func (s *TradeSettlementOrderService) SettlementMarketPayOrder(ctx context.Context, tradePaySuccessEntity *model.TradePaySuccessEntity) (*model.TradePaySettlementEntity, error) {
+	log.Printf("拼团交易-支付订单结算: userId=%s outTradeNo=%s", tradePaySuccessEntity.UserId, tradePaySuccessEntity.OutTradeNo)
+
+	// 1. 查询拼团信息
+	marketPayOrderEntity, err := s.repository.QueryMarketPayOrderEntityByOutTradeNo(ctx, tradePaySuccessEntity.UserId, tradePaySuccessEntity.OutTradeNo)
+	if err != nil {
+		return nil, err
+	}
+	if marketPayOrderEntity == nil {
+		log.Printf("不存在的外部交易单号或用户已退单，不需要做支付订单结算: userId=%s outTradeNo=%s", tradePaySuccessEntity.UserId, tradePaySuccessEntity.OutTradeNo)
+		return nil, nil
+	}
+
+	// 2. 查询组团信息
+	groupBuyTeamEntity, err := s.repository.QueryGroupBuyTeamByTeamId(ctx, marketPayOrderEntity.TeamId)
+	if err != nil {
+		return nil, err
+	}
+
+	// 3. 构建聚合对象
+	groupBuyTeamSettlementAggregate := &model.GroupBuyTeamSettlementAggregate{
+		UserEntity:            &model.UserEntity{UserId: tradePaySuccessEntity.UserId},
+		GroupBuyTeamEntity:    groupBuyTeamEntity,
+		TradePaySuccessEntity: tradePaySuccessEntity,
+	}
+
+	// 4. 拼团交易结算
+	err = s.repository.SettlementMarketPayOrder(ctx, groupBuyTeamSettlementAggregate)
+	if err != nil {
+		return nil, err
+	}
+
+	// 5. 返回结算信息 - 公司中开发这样的流程时候，会根据外部需要进行值的设置
+	return &model.TradePaySettlementEntity{
+		Source:     tradePaySuccessEntity.Source,
+		Channel:    tradePaySuccessEntity.Channel,
+		UserId:     tradePaySuccessEntity.UserId,
+		TeamId:     marketPayOrderEntity.TeamId,
+		ActivityId: groupBuyTeamEntity.ActivityId,
+		OutTradeNo: tradePaySuccessEntity.OutTradeNo,
+	}, nil
+}
--- internal/domain/trade/trade.go
+++ internal/domain/trade/trade.go
@@ -4,11 +4,13 @@ import (
 	"github.com/google/wire"
 	"group-buy-market-go/internal/domain/trade/biz/lock"
 	"group-buy-market-go/internal/domain/trade/biz/lock/filter"
+	"group-buy-market-go/internal/domain/trade/biz/settlement"
 )
 
 // ProviderSet for wire
 var ProviderSet = wire.NewSet(
 	lock.NewTradeLockOrder,
+	settlement.NewTradeSettlementOrderService,
 	filter.NewActivityUsabilityRuleFilter,
 	filter.NewUserTakeLimitRuleFilter,
 	filter.NewTradeRuleFilterFactory,
