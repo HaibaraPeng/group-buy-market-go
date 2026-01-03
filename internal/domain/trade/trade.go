7be15c9522a57b646a626d1bf78031586b295f89
Roc <514178739@qq.com>
2026-01-03 20:17:26 +0800

    refactor(trade): 更新依赖注入配置以支持结算规则过滤器

--- internal/domain/trade/trade.go
+++ internal/domain/trade/trade.go
@@ -3,15 +3,21 @@ package trade
 import (
 	"github.com/google/wire"
 	"group-buy-market-go/internal/domain/trade/biz/lock"
-	"group-buy-market-go/internal/domain/trade/biz/lock/filter"
+	lockfilter "group-buy-market-go/internal/domain/trade/biz/lock/filter"
 	"group-buy-market-go/internal/domain/trade/biz/settlement"
+	settlementfilter "group-buy-market-go/internal/domain/trade/biz/settlement/filter"
 )
 
 // ProviderSet for wire
 var ProviderSet = wire.NewSet(
 	lock.NewTradeLockOrder,
 	settlement.NewTradeSettlementOrderService,
-	filter.NewActivityUsabilityRuleFilter,
-	filter.NewUserTakeLimitRuleFilter,
-	filter.NewTradeLockRuleFilterFactory,
+	lockfilter.NewActivityUsabilityRuleFilter,
+	lockfilter.NewUserTakeLimitRuleFilter,
+	lockfilter.NewTradeLockRuleFilterFactory,
+	settlementfilter.NewSCRuleFilter,
+	settlementfilter.NewSettableRuleFilter,
+	settlementfilter.NewOutTradeNoRuleFilter,
+	settlementfilter.NewEndRuleFilter,
+	settlementfilter.NewTradeSettlementRuleFilterFactory,
 )
