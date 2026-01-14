2f52bd9c0ad24d0c06324c4c30cbb5c191b79fc3
Roc <rocdongai@gmail.com>
2026-01-14 20:29:47 +0800

    feat(trade): 添加回调类型枚举和回调配置值对象

--- internal/domain/trade/model/vos.go
+++ internal/domain/trade/model/vos.go
@@ -19,3 +19,23 @@ const (
 	// NULL 空值
 	NULL NotifyTaskHTTPEnumVO = "1001"
 )
+
+// NotifyTypeEnumVO 回调类型枚举值对象
+type NotifyTypeEnumVO string
+
+const (
+	// HTTP 回调方式为HTTP
+	HTTP NotifyTypeEnumVO = "HTTP"
+	// MQ 回调方式为MQ
+	MQ NotifyTypeEnumVO = "MQ"
+)
+
+// NotifyConfigVO 回调配置值对象
+type NotifyConfigVO struct {
+	// 回调方式；MQ、HTTP
+	NotifyType NotifyTypeEnumVO `json:"notifyType"`
+	// 回调消息
+	NotifyMQ string `json:"notifyMQ"`
+	// 回调地址
+	NotifyUrl string `json:"notifyUrl"`
+}
